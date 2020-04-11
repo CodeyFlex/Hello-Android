e60acd24505ecdcc8dcddcbca7475aa38d4362c4
CodeyFlex <Sej920@hotmail.com>
2020-04-11 16:34:38 +0200

    3. (MVVM Demo) created models package and class, and gave views it's own package.

---
+++ app/src/main/java/com/example/hello_android/Models/Model.java
@@ -0,0 +1,4 @@
+package com.example.hello_android.Models;
+
+public class Model {
+}
--- app/src/main/java/com/example/hello_android/View/MainActivity.java
+++ app/src/main/java/com/example/hello_android/View/MainActivity.java
@@ -1,10 +1,11 @@
-package com.example.hello_android;
+package com.example.hello_android.View;
 
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.hello_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
