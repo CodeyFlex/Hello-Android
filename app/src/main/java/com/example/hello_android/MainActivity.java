0242db24b3cbde4c320156f76d49fb9dd35004ef
CodeyFlex <Sej920@hotmail.com>
2020-04-11 13:34:15 +0200

    2. Added UI for assigment (Textview, EditText & Button with functionality)

--- app/src/main/java/com/example/hello_android/MainActivity.java
+++ app/src/main/java/com/example/hello_android/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.hello_android;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //This method will change the text of my main text view
+    public void changeText(View view) {
+        TextView main_text_view = (TextView) findViewById(R.id.main_text_view);
+        EditText plain_text_input = (EditText) findViewById(R.id.plain_text_input);
+        main_text_view.setText("" + plain_text_input.getText());
     }
 }
